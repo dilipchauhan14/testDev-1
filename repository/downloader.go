740a84172e0227968499d428a2764e8b86a3d56b
dilipchauhan14 <59637879+dilipchauhan14@users.noreply.github.com>
2020-01-08 14:21:58 +0530

    Update downloader.go
    Added meaningful comments.

--- repository/downloader.go
+++ repository/downloader.go
@@ -11,6 +11,8 @@ import (
 	"time"
 )
 
+
+// All the information about a specific download request
 type DownloadInformation struct {
 	ID            string
 	StartTime     time.Time
@@ -47,6 +49,7 @@ func New(request DownloadRequest) *DownloadInformation {
 	return information
 }
 
+// To begin download process
 func (information DownloadInformation) Download() {
 
 	information.markDownloadStart()
@@ -59,6 +62,7 @@ func (information DownloadInformation) Download() {
 
 }
 
+// Downloads files serially
 func (information DownloadInformation) serialDownloader() {
 
 	for _, url := range information.URLs {
@@ -75,6 +79,7 @@ func (information DownloadInformation) serialDownloader() {
 	information.markDownloadEnd(SUCCESS)
 }
 
+// Download Files in a concurrengt manner.
 func (information DownloadInformation) concurrentDownloader() {
 
 	var wg sync.WaitGroup
@@ -89,7 +94,6 @@ func (information DownloadInformation) concurrentDownloader() {
 	information.markDownloadEnd(SUCCESS)
 }
 
-// Handles Concurrent Download Requests
 func (information DownloadInformation) concurrentDownloadHandler(url string, wg *sync.WaitGroup) {
 
 	filePath := information.DirectoryPath + getFileNameFromURL(url)
@@ -105,6 +109,7 @@ func (information DownloadInformation) concurrentDownloadHandler(url string, wg 
 	information.appendDownloadFile(url, filePath)
 }
 
+// Downloads a specific File.
 func downloadFile(url string, filePath string) error {
 
 	out, err := os.Create(filePath)
