99b5cb06d380aa9b543895ee929d2bff09d7b8b8
Dilip kumar chauhan <39065549+Dilipchauhan1998@users.noreply.github.com>
2020-01-08 12:16:44 +0530

    updated apiResponse.go
    added comments for the functions

--- apiResponse.go
+++ apiResponse.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/sammarth-kapse/FileDownloadManager/repository"
 
+//getDownloadID returns string
+// get the dowmload id of a dowmload request
 func getDownloadID(downloadRequest repository.DownloadRequest) string {
 
 	downloadInformation := repository.New(downloadRequest)
@@ -11,6 +13,8 @@ func getDownloadID(downloadRequest repository.DownloadRequest) string {
 	return downloadInformation.ID
 }
 
+//getDownloadInfomationID
+//get the information of a download from itd dowmload id
 func getDownloadInformationByID(id string) (*repository.DownloadInformation, bool) {
 
 	return repository.GetDownloadInformationByID(id)
